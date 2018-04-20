3c6146091393f3c3eb3665753735b1da9d8bed86
Justin Holzmann <jholzmann@8thlight.com>
2018-04-20 15:32:41 -0500

    Make calls in real time

--- app/src/index.js
+++ app/src/index.js
@@ -13,13 +13,46 @@ function setup() {
   document.getElementsByTagName("head")[0].appendChild(cssElement);
 }
 
-function loadJSONP(url) {
+function loadJSONP(settings) {
+  const url = settings.url;
+  const callbackName = settings.callbackName;
+
   const script = document.createElement("script");
   script.type = "text/javascript";
   script.src = url;
 
-  const head = document.getElementsByTagName("head")[0];
-  head.appendChild(script);
+  window[callbackName] = data => {
+    window[callbackName].data = data;
+  };
+
+  return Observable.create(observer => {
+    const handler = e => {
+      const status = e.type === "error" ? 400 : 200;
+      const response = window[callbackName].data;
+
+      if (status === 200) {
+        observer.next({
+          status,
+          responseType: "jsonp",
+          response,
+          originalEvent: e
+        });
+
+        observer.complete();
+      } else {
+        observer.error({
+          type: "error",
+          status,
+          originalEvent: e
+        });
+      }
+    };
+
+    script.onload = script.onreadystatechanged = script.onerror = handler;
+
+    const head = window.document.getElementsByTagName("head")[0];
+    head.insertBefore(script, head.firstChild);
+  });
 }
 
 setup();
@@ -33,13 +66,16 @@ document.body.appendChild(mapContainer);
 const map = L.map("map").setView([33.858631, -118.279602], 7);
 L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png").addTo(map);
 
-const quakes$ = Observable.create(observer => {
-  window.eqfeed_callback = response => {
-    response.features.forEach(feature => observer.next(feature));
-  };
-
-  loadJSONP(QUAKE_URL);
-});
+const quakes$ = Observable.interval(5000)
+  .startWith(0)
+  .flatMap(() => {
+    return loadJSONP({
+      url: QUAKE_URL,
+      callbackName: "eqfeed_callback"
+    }).retry(3);
+  })
+  .flatMap(result => Observable.from(result.response.features))
+  .distinct(quake => quake.properties.code);
 
 quakes$.subscribe(quake => {
   const coords = quake.geometry.coordinates;
